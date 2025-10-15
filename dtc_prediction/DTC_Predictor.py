2ea2dbbe1139d6ebe6d4caf9db56b5cb7f174be3
Yonatan018 <47930108+Yonatan018@users.noreply.github.com>
2025-10-15 14:20:11 +0200

    Update DTC_Predictor.py

--- dtc_prediction/DTC_Predictor.py
+++ dtc_prediction/DTC_Predictor.py
@@ -4,28 +4,24 @@ from __future__ import annotations
 from .preprocess import transform_df
 import sys
 from pathlib import Path
-import json
+from dtc_prediction import DTC_Config
 
 PARENT = Path(__file__).resolve().parent.parent if "__file__" in globals() else Path.cwd().parent
 sys.path.insert(0, str(PARENT))
 
 from root_dir import ROOT_DIR
+
 from os import path
-DEF_THRESH = 0.5
-def _load_threshold() -> float:
-    hpo_path= path.join(ROOT_DIR,"models/hpo_summary.json")
-    with hpo_path.open("r", encoding="utf-8") as f:
-        hpo = json.load(f)
-    
-    return hpo["chosen_threshold"]
+
+
+THRESHOLD = DTC_Config.load_threshold()
 
 def predict(data, model, scaler, seq_len=48):
-    features = ['SOC', 'SOH', 'Charging_Cycles', 'Battery_Temp', 'Motor_RPM', 'Motor_Torque', 'Motor_Temp', 'Brake_Pad_Wear', 'Tire_Pressure']
-    threshold = _load_threshold(DEF_THRESH)
-
+    features = ['SOC', 'SOH', 'Charging_Voltage', 'Battery_Temp', 'Motor_RPM', 'Motor_Torque', 'Motor_Temp', 'Brake_Pad_Wear', 'Tire_Pressure']
+    
     data = transform_df(data, scaler)
     X = data[features].tail(seq_len).to_numpy(dtype="float32")[None, ...]
     prob = float(model.predict(X, verbose=0).ravel()[0])
-    label = int(prob >= threshold)
+    label = int(prob >= THRESHOLD)
 
     return prob, label
